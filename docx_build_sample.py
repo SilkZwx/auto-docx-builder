bcd33246cb5c4b5e826bb108b73d631086113123
SilkZwx <silkspheresilky@gmail.com>
2023-11-21 01:49:44 +0900

    docxに記述する内容を追加

--- docx_build_sample.py
+++ docx_build_sample.py
@@ -1,18 +1,38 @@
 import os
 from docx import Document
 from dotenv import load_dotenv
+from docx.enum.text import WD_ALIGN_PARAGRAPH
+from docx.shared import Pt, Mm
 
 
 def main():
     user = os.getenv("USERNAME")
     file = Document()
 
-    file.add_heading("Sample Document", 0)
-    file.add_paragraph("This is a sample paragraph.")
-    file.add_paragraph("This is another sample paragraph.")
-    file.add_page_break()
-    file.add_paragraph("This is a paragraph on the second page.")
-    file.save("sample.docx")
+    # ファイルの余白を設定
+    section = file.sections[0]
+    section.top_margin = Mm(30)
+    section.bottom_margin = Mm(20)
+    section.left_margin = Mm(20)
+    section.right_margin = Mm(16)
+
+    paragraph_format = file.styles["Normal"].paragraph_format
+    # デフォルトの段落スペースを0ポイントに設定
+    paragraph_format.space_before = Pt(0)
+    paragraph_format.space_after = Pt(0)
+
+    p = file.add_paragraph("報告日：2023/xx/xx")
+    p.alignment = WD_ALIGN_PARAGRAPH.RIGHT
+    p.paragraph_format.right_indent = Pt(30)
+
+    file.add_paragraph()
+    p = file.add_paragraph("学籍番号：xxxxxx")
+    p.alignment = WD_ALIGN_PARAGRAPH.RIGHT
+    p.paragraph_format.right_indent = Pt(75)
+    p = file.add_paragraph("氏名：xxxxxx")
+    p.alignment = WD_ALIGN_PARAGRAPH.RIGHT
+    p.paragraph_format.right_indent = Pt(75)
+
     file.save("/mnt/c/Users/" + user + "/Downloads/sample.docx")
 
 
